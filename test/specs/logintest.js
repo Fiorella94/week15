dccaf3d648661fe0acfdc70867e72419a74c55e8
fiorella94 <fiorella1994_s@hotmail.com>
2021-05-28 10:31:05 -0300

    tests added.

--- test/specs/logintest.js
+++ test/specs/logintest.js
@@ -1,5 +1,6 @@
 const LoginPage = require('../pageobjects/login.page');
 const InventoryPage = require('../pageobjects/inventory.page');
+const { url } = require('inspector');
 describe (
     'login section with two input fields, one login button, three hidden close buttons'+
     ', a credentials field and two images(one as a logo and one as a src image',  () => {    
@@ -45,4 +46,77 @@ describe (
             browser.pause(1000);            
         }); 
     });
-    
+    describe ('usernames and password testing', () => {
+        beforeAll('clean browser', () => {
+            browser.refresh();
+        });   
+        it('empty username and emty password' , () => {
+            LoginPage.testLogin('', '');
+            LoginPage.loginBtn.click();
+            expect(LoginPage.wrongUserInput).toBeDisplayed();
+            expect(LoginPage.errorMessageContainer)
+            .toHaveText("Epic sadface: Username is required");
+            browser.url(urlLogin);
+        });   
+        it('valid username and empty password' , () => {
+            LoginPage.testLogin('standard_user' , '');
+            LoginPage.loginBtn.click();
+            expect(LoginPage.wrongUserInput).toBeDisplayed();
+            expect(LoginPage.errorMessageContainer)
+            .toHaveText("Epic sadface: Password is required");
+            browser.url(urlLogin);
+        });
+        it('valid username and wrong password' , () => {
+            LoginPage.testLogin('standard_user' , '12345');
+            LoginPage.loginBtn.click();
+            expect(Llogin.wrongUserInput).toBeDisplayed();
+            expect(LoginPage.errorMessageContainer)
+            .toHaveText("Epic sadface: Username and password do not match any user in this service");
+            browser.url(urlLogin);
+        });
+        it('invalid username and invalid password', () => {            
+            LoginPage.testLogin('user1', '12345');
+            LoginPage.loginBtn.click();
+            expect(LoginPage.wrongUserInput).toBeDisplayed();
+            expect(LoginPage.errorMessageContainer)
+            .toHaveText(
+                "Epic sadface: Username and password do not match any user in this service");
+            });
+         it('locked user with correct password we stay on the'+
+            'login page and get an error message', () => {            
+            LoginPage.testLogin('locked_out_user', 'secret_sauce');
+            LoginPage.loginBtn.click();
+            expect(LoginPage.wrongUserInput).toBeDisplayed();
+            expect(LoginPage.errorMessageContainer)
+            .toHaveText("Epic sadface: Sorry, this user has been locked out.");                                  
+        });
+        it('standar  username and password, loads the next url all'+
+        ' the pictures are not the dog ones', () => {            
+            LoginPage.testLogin('standard_user', 'secret_sauce');
+            expect(LoginPage.wrongUserInput).not.toBeDisplayed();           
+            expect(browser).toHaveUrl(urlInventory);            
+            browser.url(urlLogin);                
+        }); 
+        it('problematic username and password, loads the next url all'+
+        ' the pictures are  dog ones', () => {                        
+            LoginPage.testLogin('problem_user', 'secret_sauce');
+            expect(LoginPage.wrongUserInput).not.toBeDisplayed();
+            expect(browser).toHaveUrl(urlInventory);
+            expect(InventoryPage.igmItemSelector(1).getAttribute('src')).toBe(urlDogImg);
+            browser.url(urlLogin);                         
+        });
+        it('performance glitch user out username and password, loads the next url all'+
+        ' the pictures are not the dog ones but it takes a long time to do it', () => {            
+            LoginPage.testLogin('performance_glitch_user', 'secret_sauce');
+            expect(LoginPage.wrongUserInput).not.toBeDisplayed();        
+            browser.setTimeout({
+                'pageLoad': 5000,
+            });
+            expect(browser).toHaveUrl(urlInventory);
+            browser.url(urlLogin);               
+        });
+    });   
+});
+
+        
+
